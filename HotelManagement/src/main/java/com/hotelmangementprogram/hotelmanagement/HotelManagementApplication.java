c74607977fd5a9994eef2b022210a8e4752097de
Dawid Błaszczyk <skiperpol1@gmail.com>
2024-01-01 18:09:24 +0100

    Added NextDay

--- HotelManagement/src/main/java/com/hotelmangementprogram/hotelmanagement/HotelManagementApplication.java
+++ HotelManagement/src/main/java/com/hotelmangementprogram/hotelmanagement/HotelManagementApplication.java
@@ -35,7 +35,37 @@ public class HotelManagementApplication {
 	 * Simulates the transition to the next day.
 	 */
 	public void nextDay(){
+		this.currentDate = this.currentDate.plusDays(1);
 
+//		TEST EMPLOYEE
+//		Employee [] employee = new Employee[1];
+//		employee[0] = new Admin(1L, "Adam", "Kowalski", "12345678901", "123456789", "adam.kowalski@email.com", Job.ADMIN, 5000.02f);
+//		Admin.payoutPaycheck(employee[0]);
+
+//		Trzeba wstawić wszystkich employee z bazy danych do listy employeesList
+//		employeesList = wszyscy employee
+//		if (this.currentDate.getDayOfMonth() == 11){
+//			for (Employee employee:employeesList) {
+//				Admin.payoutPaycheck(employee);
+//			}
+//		}
+
+
+//		TEST ROOM
+		Room [] rooms = new Room[1];
+		rooms[0] = new Room(null, 208L, 400.0, true, true, RoomType.DOUBLEROOM, null);
+//		TEST GUEST
+		Guest [] guests = new Guest[1];
+		guests[0] = new Guest(null, "Piotr", "Wiśniewski", "34567890123", "456789012", "piotr.wisniewski@email.com", 0F, 208L, LocalDate.of(2023, 11, 12), LocalDate.of(2024, 1, 1));
+//		guestsList = wszyscy goście których checkoutDate != null
+		for (Guest guest:guests) {
+			if (guest.getCheckOutDate()==currentDate){
+//				roomTemp = pokój o numerze równym numerze u guesta
+//				balance += guest.getAdditionalCharges() + roomTemp.getRoomPrice;
+//				roomTemp.setRoomIsEmpty = True;
+//				roomTemp.isClean = False;
+			}
+		}
 	}
 
 	public static void main(String[] args) {SpringApplication.run(HotelManagementApplication.class, args);}
