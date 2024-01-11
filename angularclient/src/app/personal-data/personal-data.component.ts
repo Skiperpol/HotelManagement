b21bc1910ecf87d6052e42aa49a608e3d045025f
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2024-01-11 10:45:35 +0100

    added recovery of sent Id for all existing pages

--- angularclient/src/app/personal-data/personal-data.component.ts
+++ angularclient/src/app/personal-data/personal-data.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-personal-data',
   templateUrl: './personal-data.component.html',
   styleUrls: ['./personal-data.component.css']
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit{
+  constructor(private router: Router){
+    console.log(this.router.getCurrentNavigation()?.extras.state);
+  }
+  public employeeId: number = 1;
+  ngOnInit(): void {
+  this.employeeId = history.state;
+  console.log(this.employeeId);
+  }
 
 }
